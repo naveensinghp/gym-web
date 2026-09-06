213e08e932d3aff7e37959785927a09f15d5737e
NaveenSingh <naveensingh.mtp@gmail.com>
2026-09-06 19:11:11 +0530

    no message

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -3,7 +3,24 @@ import styled from 'styled-components';
 
 
 function Card() {
-  return <div>I am Card</div>;
+  return <>
+    <Wrapper>
+      Workout Split
+    </Wrapper>
+  </>;
 }
 
 export default Card;
+
+const Wrapper = styled.div`
+  width: 300px;
+  height: 200px;
+  background-color: #f4f7fb;
+  border-radius: 8px;
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 1.5rem;
+  color: #333;
+`
