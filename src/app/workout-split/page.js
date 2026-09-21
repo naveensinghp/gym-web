620f69fcec3858c1772c4b69bc6065b77c07c9a5
NaveenSingh <naveensingh.mtp@gmail.com>
2026-09-21 23:29:22 +0530

    no message

--- src/app/workout-split/page.js
+++ src/app/workout-split/page.js
@@ -11,24 +11,47 @@ export default function WorkoutSplit() {
         <>
             {/* <Card></Card> */}
             <Wrapper>
-                <TitleWrapper>
-                    <Title>Workout Split</Title>
-                    <SmallText>Follow a structured workout split to target each muscle group and achieve your fitness goals effectively.</SmallText>
-                </TitleWrapper>
+                <LeftSection>
+                    <TitleWrapper>
+                        <Title>Workout Split</Title>
+                        <SmallText>Follow a structured workout split to target each muscle group and achieve your fitness goals effectively.</SmallText>
+                    </TitleWrapper>
+                </LeftSection>
+                <RightSection>
+                  <Rwrapper>
+                    Naveen
+                  </Rwrapper>
+                </RightSection>
             </Wrapper>
         </>
     );
 }
 
 const Wrapper = styled.div`
+    display: flex;
+    justify-content: space-between;
     padding: 20px;
 `;
 
+
+const LeftSection = styled.div`
+`
+
+const RightSection = styled.div`
+
+`
+
+const Rwrapper = styled.div`
+    background: white;
+    padding: 20px;
+`
+
+
 const TitleWrapper = styled.div`
-  display: flex;
+  /* display: flex;
   flex-direction: column;
   align-items: baseline;
-  gap: 20px;
+  gap: 20px; */
 `;
 
 const Title = styled.div`
